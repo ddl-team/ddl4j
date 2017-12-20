4b05068e63665ed3d69b56182731827c66cd09cc
Kirill Batalin <kir55rus@gmail.com>
2017-12-21 01:58:07 +0700

    chore(Maven) Добавление тестовых данных

---
+++ src/main/java/ru/nsu/ddlteam/Main.java
@@ -0,0 +1,11 @@
+package ru.nsu.ddlteam;
+
+/**
+ * Тестовый класс. Удалить после создания нормальной архитектуры
+ * todo
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello!");
+    }
+}
